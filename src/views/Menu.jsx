289d6e59f7ee96ec01b6778e4d1ce83af5b1e46d
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-12-07 17:52:12 -0300

    starting to style category pages

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import wallpaperCharacters from "../assets/fondo1.png";
+import wallpaperSeries from "../assets/fondo2.png";
+import wallpaperComics from "../assets/fondo4.png";
 // import { UseApiContext } from "../context/ApiContext";
 
 const Menu = () => {
@@ -11,32 +14,44 @@ const Menu = () => {
   //     })
   // }, []);
 
+  const [wallpaper, setWallpaper] = useState("");
+
   const { categoryID } = useParams();
   //console.log(categoryID);
-  if(categoryID){
-    const links = document.getElementsByClassName('navbar-menu-links')     
+  if (categoryID) {
+    const links = document.getElementsByClassName("navbar-menu-links");
     for (let index = 0; index < links.length; index++) {
       const link = links[index];
-      link.classList.remove('hidden')
+      link.classList.remove("hidden");
     }
   }
 
-  switch (categoryID) {
-    case "characters":
-      console.log(`The selected category is: ${categoryID}`)
-      break;
-    case "comics":
-      console.log(`The selected category is: ${categoryID}`)
-      break;
-    case "series":
-      console.log(`The selected category is: ${categoryID}`)
-      break;
-    default:
-      console.log(`No category selected`)
-      break;
-  }
+  useEffect(() => {
+    switch (categoryID) {
+      case "characters":
+        setWallpaper(wallpaperCharacters);
+        //console.log(`The selected category is: ${categoryID}`);
+        break;
+      case "comics":
+        setWallpaper(wallpaperComics);
+        //console.log(`The selected category is: ${categoryID}`);
+        break;
+      case "series":
+        setWallpaper(wallpaperSeries);
+        //console.log(`The selected category is: ${categoryID}`);
+        break;
+      default:
+        setWallpaper(wallpaperCharacters);
+        break;
+    }
+  }, [categoryID]);
 
-  return <div className="menu-container"></div>;
+  return (
+    <div className="menu-container">
+      <img className="menu-container-wallpaper" src={wallpaper} alt="" />
+      <h1>{categoryID}</h1>
+    </div>
+  );
 };
 
 export default Menu;
