25b4771c4b0d54af870713df65fbaae730bd1507
TimoteoGambella <timi.gambella@hotmail.com>
2022-12-06 23:41:02 -0300

    ApiContext

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import "./styles/styles.scss"
+import { ApiContext } from "./context/ApiContext.jsx";
 
 function App() {
   return (
     <>
+    <ApiContext>
       <NavBar/>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
+    </ApiContext>
     </>
   );
 }
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -4,36 +4,38 @@ export const UseApiContext = createContext();
 
 export const ApiContext = ({children}) => {
 
+    // LLAMADO A MARVEL API
     const apiFetch1=async(url)=>{
-        await fetch(
-            `${url}`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              return(data)
-            })
-            .catch((error) => {
-              console.log(error);
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
+      await fetch(
+          `${url}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            return(data)
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
     }
 
+    // LLAMADO A SUPERHERO API
     const apiFetch2=async(id)=>{
-        await fetch(
-            `https://superheroapi.com/api/5651474918252451/${id}`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              return(data)
-            })
-            .catch((error) => {
-              console.log(error);
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
+      await fetch(
+          `https://superheroapi.com/api/5651474918252451/${id}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            return(data)
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
     }
 
     return(
