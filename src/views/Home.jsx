0b6879aeca05e96e8bcb937132e15f62288d5a3f
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-12-07 08:43:27 -0300

    adding main text to hero section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -13,7 +13,10 @@ const Home = ()=>{
 
     return(
         <div className="home-container">
-        
+            <div className="font-family-marvel-roboto">
+                <h1 className="hero-text-main">ARE YOU A MARVEL FAN?</h1>
+                <h3 className="hero-text-mainsub">Get into the section that you are most interested, and enjoy the Marvel world</h3>
+            </div>
         </div>
     )
 }
