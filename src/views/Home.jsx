c83477be5446a4708f698ab056451879530078b9
TimoteoGambella <timi.gambella@hotmail.com>
2022-12-06 23:41:09 -0300

    Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+// import { UseApiContext } from "../context/ApiContext";
 
 const Home = ()=>{
 
+    // const {apiFetch1}=useContext(UseApiContext)
+
+    // useEffect(() => {
+    //     apiFetch1("https://odshjfdsfñsldfdsfsdf").then((res)=>{
+    //         console.log(res)
+    //     })
+    // }, []);
+
     return(
         <div className="home-container">
-            
+        
         </div>
     )
 }
