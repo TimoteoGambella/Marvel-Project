52d79533b6a62457dfddff7b31e7627cd10ae2bc
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-12-07 20:01:21 -0300

    cards container & cards ready (for now)

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React from "react";
+import MenuListCard from "../MenuListCard/MenuListCard";
 
 const MenuList = () => {
   return (
-    <div className='menu-list-card'>
-      <div className='menu-list-card-body'>
-        <img className='menu-list-card-image' src="https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg" alt="" />
-      </div>
-      <div className='menu-list-card-footer'>
-        <p>Heroe Name</p>
-      </div>
+    <div className="menu-list-container">
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
+      <MenuListCard
+        heroImage={
+          "https://static01.nyt.com/images/2022/10/31/business/00Superheroes-print/00Superheroes-black-adam-mediumSquareAt3X.jpg"
+        }
+        heroName={"Smash"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MenuList
+export default MenuList;
---
+++ src/components/MenuListCard/MenuListCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MenuListCard = ({heroImage, heroName }) => {
+  return (
+    <div className="menu-list-card">
+      <div className="menu-list-card-body">
+        <img
+          className="menu-list-card-image"
+          src={heroImage}
+          alt=""
+        />
+      </div>
+      <div className="menu-list-card-footer font-family-marvel-roboto">
+        <p>{heroName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MenuListCard;
