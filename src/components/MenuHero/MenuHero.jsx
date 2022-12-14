4df824427c5435eb004869686638f9d167e81616
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-12-14 18:48:52 -0300

    cambiando wallpaper segun categoryID working, ahora darle estillo

--- src/components/MenuHero/MenuHero.jsx
+++ src/components/MenuHero/MenuHero.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import wallpaperCharacters from "../../assets/fondo1.png";
+import wallpaperComics from "../../assets/fondo4.png";
+import wallpaperSeries from "../../assets/fondo2.png";
 
 const MenuHero = ({ categoryID }) => {
+  const [menuHeroWallpaper, setMenuHeroWallpaper] = useState("");
+
+  useEffect(() => {
+    if (categoryID === "characters") {
+      setMenuHeroWallpaper(wallpaperCharacters);
+    } else if (categoryID === "comics") {
+      setMenuHeroWallpaper(wallpaperComics);
+    } else if (categoryID === "series") {
+      setMenuHeroWallpaper(wallpaperSeries);
+    }
+  }, [categoryID]);
+
   return (
-    <div className="menu-container-hero">
+    <div id="menu-container" className="menu-container-hero">
+      <img src={menuHeroWallpaper} alt="" />
       <div className="menu-container-hero-text">
         <h1 className="font-family-marvel-principal" id="header">
           marvel {categoryID}
