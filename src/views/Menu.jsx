d3c8222b0a43d30271472e8d868568c4706c95f1
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-12-28 15:45:28 -0300

    empezando a fetchear la data

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import MenuContainer from "../components/MenuContainer/MenuContainer";
-// import { UseApiContext } from "../context/ApiContext";
 
 const Menu = () => {
-  // const {apiFetch1}=useContext(UseApiContext)
-
-  // useEffect(() => {
-  //     apiFetch1("https://odshjfdsfñsldfdsfsdf").then((res)=>{
-  //         console.log(res)
-  //     })
-  // }, []);
-
-  return (
-    <MenuContainer />
-  );
+  return <MenuContainer />;
 };
 
 export default Menu;
