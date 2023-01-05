1cdb5426904a2e6344c7e1b66956ac098d7cb5ad
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-05 14:54:50 -0300

    acomodando card, avanzada

--- src/components/FooterWorkerCard/FooterWorkerCard.jsx
+++ src/components/FooterWorkerCard/FooterWorkerCard.jsx
@@ -1,29 +1,38 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Link,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
-const FooterWorkerCard = ({image, name, job, linkedin}) => {
+const FooterWorkerCard = ({ image, name, job, linkedin }) => {
   return (
     <Box>
       <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={image}
-        title={`${name} Photo`}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {job}
-        </Typography>
-      </CardContent>
-      <CardActions >
-        <Button size="small"><a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></Button>
-      </CardActions>
-    </Card>
+        <CardMedia sx={{ height: 200 }} image={image} title={`${name} Photo`} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {job}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">
+            <Link underline="none" href={linkedin} target="_blank" rel="noopener noreferrer">
+              LinkedIn Profile
+            </Link>
+          </Button>
+        </CardActions>
+      </Card>
     </Box>
-  )
-}
+  );
+};
 
-export default FooterWorkerCard
+export default FooterWorkerCard;
