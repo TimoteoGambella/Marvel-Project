ab586fbf5466c598f8052058d071f1da24ffc22b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-05 13:15:28 -0300

    agregando modal de material para el footer

--- src/components/FooterSocials/FooterSocials.jsx
+++ src/components/FooterSocials/FooterSocials.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import { Box, Button, Modal, Typography } from "@mui/material";
+import React from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const FooterSocials = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className='footer-socials'>
-      <div className='footer-socials-logos'>
-        <a target={"_blank"} href="https://www.linkedin.com/" rel='noopener noreferrer' ><i className="fa-brands fa-linkedin-in"></i></a>
-        <i id='socials-divider' className="fa-solid fa-minus"></i>
-        <a target={"_blank"} href="https://www.behance.net/" rel='noopener noreferrer' ><i className="fa-brands fa-behance"></i></a>
+    <div className="footer-socials">
+      <div className="footer-socials-logos">
+        <Button onClick={handleOpen}>
+          <i className="fa-brands fa-linkedin-in"></i>
+          <i id="socials-divider" className="fa-solid fa-minus"></i>
+          <i className="fa-brands fa-behance"></i>
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterSocials
+export default FooterSocials;
