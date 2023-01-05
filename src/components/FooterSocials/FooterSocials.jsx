abfd32cf9261e833988fd81e21d476a163103a34
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-05 14:58:33 -0300

    agregando data de cada uno de los workers

--- src/components/FooterSocials/FooterSocials.jsx
+++ src/components/FooterSocials/FooterSocials.jsx
@@ -49,10 +49,10 @@ const FooterSocials = () => {
           </Typography>
           <Box id="modal-modal-description" display="flex" gap='10px' sx={{ mt: 2 }}>
             <FooterWorkerCard
-              image="https://media.licdn.com/dms/image/C4D03AQG7AMvyKJoMfQ/profile-displayphoto-shrink_200_200/0/1654825100218?e=1678320000&v=beta&t=3AKM3ZNuhzlhYRsdujLY-h32coTVeVu2YPh4-OFjRxM"
+              image="https://media.licdn.com/dms/image/C4D03AQHjMQhlIKIS4g/profile-displayphoto-shrink_200_200/0/1625678725840?e=1678320000&v=beta&t=d59LTGCF9cchaX2KR5zYo87Cc3ex2vX2g2m-6nnIvX4"
               job="Developer"
-              linkedin="https://www.linkedin.com/in/patoalbornoz/"
-              name="Patricio Albornoz"
+              linkedin="https://www.linkedin.com/in/timoteo-gambella-4b6418210/"
+              name="Timoteo Gambella"
             />
             <FooterWorkerCard
               image="https://media.licdn.com/dms/image/C4D03AQG7AMvyKJoMfQ/profile-displayphoto-shrink_200_200/0/1654825100218?e=1678320000&v=beta&t=3AKM3ZNuhzlhYRsdujLY-h32coTVeVu2YPh4-OFjRxM"
@@ -61,16 +61,16 @@ const FooterSocials = () => {
               name="Patricio Albornoz"
             />
             <FooterWorkerCard
-              image="https://media.licdn.com/dms/image/C4D03AQG7AMvyKJoMfQ/profile-displayphoto-shrink_200_200/0/1654825100218?e=1678320000&v=beta&t=3AKM3ZNuhzlhYRsdujLY-h32coTVeVu2YPh4-OFjRxM"
-              job="Developer"
-              linkedin="https://www.linkedin.com/in/patoalbornoz/"
-              name="Patricio Albornoz"
+              image="https://media.licdn.com/dms/image/C4D03AQGkZYbT_MwRKQ/profile-displayphoto-shrink_200_200/0/1642646030689?e=1678320000&v=beta&t=wVRFOpFfTVFoEiMoU9ueGUDS_7_-5_lVhD6fANEdrzE"
+              job="UX UI Designer"
+              linkedin="https://www.linkedin.com/in/candelaria-guzm%C3%A1n-65b594203/"
+              name="Candelaria Guzmán"
             />
             <FooterWorkerCard
-              image="https://media.licdn.com/dms/image/C4D03AQG7AMvyKJoMfQ/profile-displayphoto-shrink_200_200/0/1654825100218?e=1678320000&v=beta&t=3AKM3ZNuhzlhYRsdujLY-h32coTVeVu2YPh4-OFjRxM"
-              job="Developer"
-              linkedin="https://www.linkedin.com/in/patoalbornoz/"
-              name="Patricio Albornoz"
+              image="https://media.licdn.com/dms/image/C4D03AQFHv2ZYZak7Iw/profile-displayphoto-shrink_200_200/0/1632952313097?e=1678320000&v=beta&t=2vIwOkMAy8oKhok_4mOtRci1_rahdWes9F8HtNGD4Ys"
+              job="UX UI Designer"
+              linkedin="https://www.linkedin.com/in/sof%C3%ADa-ocampo-roure-5b7236170/"
+              name="Sofía Ocampo Roure"
             />
           </Box>
         </Box>
