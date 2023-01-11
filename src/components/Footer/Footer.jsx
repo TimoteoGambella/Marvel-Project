7bb0c070d5bb96a8e60cd10f8a034e8227fde575
TimoteoGambella <timi.gambella@hotmail.com>
2023-01-11 10:23:47 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterText from "../FooterText/FooterText";
 
 const Footer = () => {
   return (
-    <div className="footer-container d-flex-row font-family-marvel-inter">
+    <div className={`footer-container d-flex-row font-family-marvel-inter ${window.location.pathname!=="/"?"footer-position-relative":"footer-position-fixed"}`}>
       <FooterText text={"Know about the page's creators"}></FooterText>
     </div>
   );
